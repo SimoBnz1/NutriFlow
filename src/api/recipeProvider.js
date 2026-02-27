6a29fce80ae2f8f4bf172ab28ea7ac9c1aceebe7
mohamed_bnz <simobnz81@gmail.com>
2026-02-27 14:28:06 +0000

    ajouter la logique pour recupirer les recettes via api

---
+++ src/api/recipeProvider.js
@@ -0,0 +1,17 @@
+const API_URL = 'https://dummyjson.com/recipes';
+
+export async function getAllRecipes() {
+  try {
+    const res = await fetch(API_URL);
+    const data = await res.json();
+    return data.recipes; 
+  } catch (err) {
+    console.error('Errore API:', err);
+    return [];
+  }
+}
+
+export async function searchRecipes(query) {
+  const recipes = await getAllRecipes();
+  return recipes.filter(r => r.name.toLowerCase().includes(query.toLowerCase()));
+}
